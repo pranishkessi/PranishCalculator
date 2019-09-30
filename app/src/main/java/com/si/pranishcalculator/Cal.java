4e540252043788a43c76b7dad04d0846d26525dd
Pranish Kessi <pranishkessi10@gmail.com>
2019-09-30 09:32:50 +0545

    Initial commit

---
+++ app/src/main/java/com/si/pranishcalculator/Cal.java
@@ -0,0 +1,12 @@
+package com.si.pranishcalculator;
+
+public interface Cal {
+    public String add(Numbers n);
+
+    public String sub(Numbers n);
+
+    public String divide(Numbers n);
+
+    public String mul(Numbers n);
+
+}
